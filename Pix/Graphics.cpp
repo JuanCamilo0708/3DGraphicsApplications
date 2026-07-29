d6047edb3b60ec0d2cc2511a11fdc2fe9d3a5b19
Juan Camilo <juanchodoman@hotmail.com>
2026-07-28 18:04:55 -0700

    Matrix4
    Adding Matrix4.h
    Changes to MathHelper to support Matrix4

--- Pix/Graphics.cpp
+++ Pix/Graphics.cpp
@@ -6,4 +6,74 @@ void Graphics::NewFrame()
 {
 	Viewport::Get()->OnNewFrame();
 	Clipper::Get()->OnNewFrame();
+	Matrix4 matTrans = Matrix4::Translation(4.0, 10.0, 5.0);
+	Matrix4 matRotX = Matrix4::RotationX(45 * MathHelper::DegToRad);
+	Matrix4 matRotY = Matrix4::RotationX(15 * MathHelper::DegToRad);
+	Matrix4 matFinal = matRotX * matRotY * matTrans;
+	Matrix4 matInv = MathHelper::Inverse(matFinal);
+	Matrix4 matIden = matFinal * matInv;
+	int hi = 0;
+	if (MathHelper::CheckEqual(matIden._11, 1.0f)) {
+		++hi;
+	}
+	if (MathHelper::CheckEqual(matIden._12, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._13, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._14, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._21, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._22, 1.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._23, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._24, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._31, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._32, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._33, 1.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._34, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._41, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._42, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._43, 0.0f)) {
+		++hi;
+
+	}
+	if (MathHelper::CheckEqual(matIden._44, 1.0f)) {
+		++hi;
+
+	}
 }
--- Pix/MathHelper.h
+++ Pix/MathHelper.h
@@ -2,7 +2,7 @@
 
 #include "Vector2.h"
 #include "Vector3.h"
-
+#include "Matrix4.h"
 namespace MathHelper {
 	constexpr float DegToRad = 3.1415926f / 180.0f;
 	
@@ -21,6 +21,16 @@ namespace MathHelper {
 	float Dot(const Vector3& a, const Vector3& b);
 
 	Vector3 Cross(const Vector3& a, const Vector3& b);
+
+	//Matric Functions
+	Vector3 TransformCoord(const Vector3& v, const Matrix4& m);
+	Vector3 TransformNormal(const Vector3& v, const Matrix4& m);
+
+	Matrix4 Transpose(const Matrix4& m);
+	Matrix4 Inverse(const Matrix4& m);
+
+	Matrix4 Adjoint(const Matrix4& m);
+	float Determinant(const Matrix4& m);
 
 
 
